49148c9955aa5e65d1348549371ef3c74a90c94e
Duta Narcis <narcisduta@gmail.com>
2021-09-30 23:52:22 +0300

    TemaSapt4string

---
+++ src/main/java/ro/sci/temasapt4/string.java
@@ -0,0 +1,17 @@
+package ro.sci.temasapt4;
+
+import java.util.Arrays;
+
+public class string {
+
+    public static void main(String[] args) {
+
+        String[] aSentence = new String[]{"I" + " " + "love" + " Java"};
+        for (int i = 0; i < aSentence.length; i++) {
+
+           System.out.println(aSentence[i]);
+
+        }
+
+    }
+}
